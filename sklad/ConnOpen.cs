8206bb8296aca952fcabfe30a73b0f1eb55113bf
Amangeldi <Amangeldi.9606@gmail.com>
2017-07-21 14:24:25 +0500

    Start

---
+++ sklad/ConnOpen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sklad
+{
+    public class ConnOpen
+    {
+        static string connStr = @"Data Source=AMAN\SQLEXPRESS;Initial Catalog=sklad;Integrated Security=True";
+        public SqlConnection connection = new SqlConnection(connStr);
+    }
+}
---
+++ sklad/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sklad
+{
+    public partial class Form1 : Form
+    {
+        public ConnOpen conn = new ConnOpen();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.connection.Open();
+            SqlCommand comm = new SqlCommand("SELECT * FROM dbo.Unit",conn.connection);
+            SqlDataReader reader = comm.ExecuteReader();
+            while(reader.Read())
+            {
+                this.Text = reader["unit_name"].ToString();
+            }
+            conn.connection.Close();
+        }
+    }
+}
