e69fffe35f5a4b7aaac2c84c8a539808fef1361e
Amangeldi <Amangeldi.9606@gmail.com>
2017-07-28 22:35:50 +0500

    Отчетность. Начало

--- sklad/Report.cs
+++ sklad/Report.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,12 +17,45 @@ namespace sklad
         {
             InitializeComponent();
         }
-        int responsible, product, traffic;
-        string sResponsible, sProduct, location, date, waybill;
+        int responsible, product, traffic, price, unit;
+        string sResponsible, sProduct, sUnit, location, date, waybill;
         float product_quantity;
         private void Report_Load(object sender, EventArgs e)
         {
+            ConnOpen reportLoad = new ConnOpen();
+            ConnOpen productLoad = new ConnOpen();
+            ConnOpen unitLoad = new ConnOpen();
+            reportLoad.connection.Open();
+            productLoad.connection.Open();
+            unitLoad.connection.Open();
+            SqlCommand commandResponsible = new SqlCommand("SELECT * FROM dbo.Responsibility", reportLoad.connection);
+            SqlDataReader readerResponsible = commandResponsible.ExecuteReader();
+            SqlCommand commandProduct;
+            SqlDataReader readerProduct;
+            SqlCommand commandUnit;
+            SqlDataReader readerUnit;
 
+            var columnPName = new DataGridViewColumn();
+            columnPName.HeaderText = "";
+            columnPName.Name = "productName";
+            while(readerResponsible.Read())
+            {
+                product = Convert.ToInt32(readerResponsible["product"]);
+                //-----
+                commandProduct = new SqlCommand("SELECT * FROM dbo.Product WHERE product_id LIKE '%"+product+"'", productLoad.connection);
+                readerProduct = commandProduct.ExecuteReader();
+                readerProduct.Read();
+                sProduct = readerProduct["product_name"].ToString();
+                unit = Convert.ToInt32(readerProduct["product_unit"]);
+                //------
+                commandUnit = new SqlCommand("", unitLoad.connection);
+                readerUnit = commandUnit.ExecuteReader();
+                sUnit = readerUnit["unit_name"].ToString();
+
+            }
+            reportLoad.connection.Close();
+            productLoad.connection.Close();
+            unitLoad.connection.Close();
         }
     }
 }
