4272437e15fb51ea1cc76bbc7816364186f3f7e2
Amangeldi <Amangeldi.9606@gmail.com>
2017-08-24 13:39:29 +0500

    fio girdeji

--- sklad/Report.cs
+++ sklad/Report.cs
@@ -65,6 +65,7 @@ namespace sklad
                 u = q + 1;
                 ExcelApp.Cells[4, q] = "sany";
                 ExcelApp.Cells[4, u] = "jemi bahasy";
+                ExcelApp.Cells[3, q] = RQPU["fio"].ToString();
                 cell = c[q - 1] + "4:" + c[q - 1] + "26";
                 workSheet.get_Range(cell).Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle = Excel.XlLineStyle.xlContinuous;
                 workSheet.get_Range(cell).Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle = Excel.XlLineStyle.xlContinuous;
