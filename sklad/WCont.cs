ca2f9932d9694163cc1bab0ed094a8d5496494e4
Amangeldi <Amangeldi.9606@gmail.com>
2017-10-05 17:20:47 +0500

    Потом нужно удалить этот класс

---
+++ sklad/WCont.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sklad
+{
+    public class WCont
+    {
+        
+        public string[] wayb;
+        public void add (int y, string x)
+        {
+            wayb[y] = x;
+        }
+    }
+}
